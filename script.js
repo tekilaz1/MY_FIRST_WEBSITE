228db29b8d1b23fb1711018ed4ffee4098e53e3d
tekilaz1 <tselama18@gmail.com>
2019-11-29 02:56:15 +0200

    My first KSP website
    Signed-off-by: tekilaz1 <tselama18@gmail.com>

---
+++ script.js
@@ -0,0 +1,13 @@
+window.addEventListener('load',function(event){
+
+
+		 	  var name = document.getElementById('myname').value;
+		 	  alert("we are sorry!!,  we donnot have  "+name+"!!")
+		 	  	;
+		 }
+}
+
+
+ 
+
+
